f47a7fa8119a7d4d80d844745d6b2ec3cba1ce5a
pawel <linpeiwei@f-road.com.cn>
2020-06-03 16:34:48 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk'
+      },
+      'element-ui'
+    ],
+    [
+      'component',
+      {
+        libraryName: 'vue-pawel-ui',
+        styleLibrary: {
+          name: 'themes',
+          base: false
+        }
+      },
+      'vue-pawel-ui'
+    ]
   ]
 }
---
+++ src/plugins/pawel.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import { Button } from 'vue-pawel-ui'
+
+Vue.use(Button)
